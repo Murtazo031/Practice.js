75bd131557e578e91ea5e1af030f9ecaafa09225
Murtazo031 <azimcompine@gmail.com>
2024-12-16 18:25:47 +0500

    First

---
+++ practice.js
@@ -0,0 +1,38 @@
+//yakum
+// function  modifyLast(a,b){
+//      let s = a.at(-1)
+//      let c = a.substring(0,a.length-1) 
+//      return c + s.repeat(b) 
+// }
+// console.log(modifyLast("Hello",5));
+
+//duyum
+// function  firstLast (a){
+//     let s = a.at(0) + a.at(-1)
+//     return s 
+// }
+// console.log(firstLast("hello"));
+
+//seyum
+// function changetype(a){
+//     if (typeof a=="string"){
+//         return Number(a)
+//     }
+//     else {
+//         return a.toString()
+//     }
+// }
+// console.log(typeof changetype("123"));
+
+//chorum
+// function isPlural(a){
+//     return (a.at(-1))== "s"
+// }
+// console.log(isPlural("flowers"));
+
+//panjum 
+// function reverseAndUppercase(word) {
+//     return word.split('').reverse().join('').toUpperCase();
+//   }
+//   console.log(reverseAndUppercase("molas"));
+  
